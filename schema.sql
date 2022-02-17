28e391e896884be0a6486d914b141cceedbec59a
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2022-02-17 13:32:52 +0200

    Add 'Join table' for visits

--- schema.sql
+++ schema.sql
@@ -42,3 +42,32 @@ ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals
 ADD COLUMN owner_id INT,
 ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
+--Create a table named vets 
+CREATE TABLE vets(
+   id SERIAL GENERATED ALWAYS AS IDENTITY,
+   name VARCHAR(255),
+   age INT,
+   date_of_graduation DATE,
+   PRIMARY KEY(id)
+   );
+
+
+-- Create a "join table" called specializations to handle this relationship.
+CREATE TABLE specializations (
+    species_id int,
+    vets_id int,
+    CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
+
+
+-- Create a "join table" called visits to handle this relationship, it should also keep track of the date of the visit.
+CREATE TABLE visits (
+    animals_id int,
+    vets_id int,
+    date_of_visit date,
+    CONSTRAINT fk_animals FOREIGN KEY(animals_id) REFERENCES animals(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
